303b9d83ba7a501dfda2fdabd5814460d1245666
jefrisembiring5 <jefri.123140206@student.itera.ac.id>
2025-06-02 02:07:42 +0700

    Add files via upload

--- tubes1-IF2211-bot-starter-pack-1.0.1/main.py
+++ tubes1-IF2211-bot-starter-pack-1.0.1/main.py
@@ -8,13 +8,12 @@ from game.bot_handler import BotHandler
 from game.logic.JamalKopling import GreedyJamalLogic
 from game.util import *
 from game.logic.base import BaseLogic
-from game.logic.SlametKopling import GreedySlametLogic
 
 init()
 BASE_URL = "http://localhost:3000/api"
 DEFAULT_BOARD_ID = 1
 CONTROLLERS = {
-    "Random": GreedyJamalLogic
+    "Random" : GreedyJamalLogic
 }
 
 ###############################################################################
